1b75150095418dd406f392ebac44a4ea1ac49bfd
ZAIIIKVAR <89867276+ZAIIIKVAR@users.noreply.github.com>
2022-11-26 14:05:05 +0300

    initial

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import MachinesList from './components/machines-list.vue';
+</script>
+
+<template>
+  <div>
+    <MachinesList />
+  </div>
+</template>
+
+<style scoped></style>
