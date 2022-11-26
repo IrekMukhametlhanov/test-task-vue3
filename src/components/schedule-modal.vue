a57bfa0ddef5ab518efd53922b33be3ca4c4619a
ZAIIIKVAR <89867276+ZAIIIKVAR@users.noreply.github.com>
2022-11-27 00:57:58 +0300

    modified:   src/components/schedule-modal.vue
    	modified:   package.json
    	modified:   src/App.vue
    	modified:   src/components/machine-item.vue
    	modified:   src/components/machines-list.vue
    	modified:   src/enums/tags.enum.ts
    	modified:   src/interfaces/machine-type.interface.ts
    	modified:   src/main.ts
    	modified:   src/services/machines.service.ts
    	modified:   src/services/trade-points.service.ts
    	modified:   yarn.lock
    
    	src/components/search-input.vue
    	src/interfaces/machine-joined.interface.ts
    	src/store/
    	src/utils/

--- src/components/schedule-modal.vue
+++ src/components/schedule-modal.vue
@@ -14,26 +14,51 @@ defineEmits<{
 
 <template>
   <div class="schedule-modal">
-    <p>Время работы</p>
-    <ul>
+    <p class="time">Время работы</p>
+    <ul class="ul">
       <li class="li" :key="day" v-for="(value, day) in schedule">
         {{ day }}: {{ value }}
       </li>
     </ul>
-
+    <div class="btn-wrapper">
     <button class="btn" @click="$emit('close')">Закрыть</button>
   </div>
+  </div>.
 </template>
 
 <style scoped>
  .btn{
-  text-align: center;
-  width: 80px;
+  margin: 0 auto;
+  background-color: rgb(255, 255, 255);
+  font-size: 14px;
+  border: 1px solid ;   
+  color: rgb(243, 142, 0);
+  border-radius: 10px;
+  width: 90px;
   height: 30px;
-  border-radius: 8px;
+  cursor: pointer;
  }
-
+ .btn:hover{
+  color: rgb(255, 255, 255);
+  background-color: rgb(255, 156, 17);
+ }
+ .btn-wrapper{
+   align-items: center;
+ }
+ .ul{
+  width: 200px;
+ }
  .li{
   list-style-type: none;
+ }
+
+ .schedule-modal{
+  position: absolute;
+  color: aliceblue;
+  background-color: rgb(255, 255, 255);
+  transform: scaleY(-150px);
+  border: 1px solid rgb(47, 216, 0);
+  border-radius: 10px;
+  max-width: 200px;
  }
 </style>
