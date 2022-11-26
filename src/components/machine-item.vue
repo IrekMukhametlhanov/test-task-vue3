91ec6c4bd1a0253a98234262ef3ed266e63c64d0
ZAIIIKVAR <89867276+ZAIIIKVAR@users.noreply.github.com>
2022-11-26 21:21:27 +0300

    modified:   package.json 	modified:   src/components/machine-item.vue 	modified:   src/components/schedule-modal.vue 	modified:   src/enums/tags.enum.ts 	modified:   src/interfaces/machine-type.interface.ts 	deleted:    src/store/index.ts 	modified:   yarn.lock

--- src/components/machine-item.vue
+++ src/components/machine-item.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import ScheduleModal from './schedule-modal.vue';
-
 import type { ITradePoint } from '../interfaces/trade-point.interface';
 import type { IMachine } from '../interfaces/machine.interface';
 import type { IMachineType } from '../interfaces/machine-type.interface';
@@ -38,38 +37,106 @@ onBeforeMount(async () => {
 </script>
 
 <template>
-  <div>
-    <h2>{{ machine.serialNumber }}</h2>
-    <p>{{ tradePoint && tradePoint.location.address }}</p>
-    <ul v-if="machineType">
-      <li :key="tag" v-for="tag in machineType.tags">
-        <p>{{ tag }}</p>
+  <div class="main_wrapper">
+  <div class="wrapper_items">
+<div class="top-wrapper">
+    <h2 class="serial-number">{{ machine.serialNumber }}</h2>
+    <ul class="ul-tags" v-if="machineType">
+      <li class="tags-li" :key="tag" v-for="tag in machineType.tags">
+        <p class="tags">{{ tag }}</p>
       </li>
     </ul>
-    <p>floor: {{ machine.floor }}</p>
-    <button
-      @click="
-        () => {
-          scheduleModalOpen = true;
-        }
-      "
-    >
+  </div>
+    <p class="location-address">{{ tradePoint && tradePoint.location.address }}</p>
+
+    
+    <p class="floor">Этаж: {{ machine.floor }}</p>
+
+    <button class="btn_schedule" 
+    @click="() => {
+        scheduleModalOpen = true;
+      }
+    ">
       Schedule
     </button>
-    <img :src="mapImage" />
-    <div v-if="tradePoint && scheduleModalOpen">
-      <ScheduleModal
-        :schedule="tradePoint.workingTime"
-        @close="
-          () => {
-            scheduleModalOpen = false;
-          }
-        "
-      />
+    <div class="trade-point" v-if="tradePoint && scheduleModalOpen">
+      <ScheduleModal :schedule="tradePoint.workingTime" @close="
+        () => {
+          scheduleModalOpen = false;
+        }
+      " />
     </div>
   </div>
+ <div class="image-wrapper">
+  <img :src="mapImage" class="image-map" />
+ </div>
+</div>
 </template>
 
-<style>
+<style scoped>
+  .main_wrapper{
+    display: flex;
+    align-items: center;
+  }
+  .wrapper_items{
+    display: flex;
+    height: 37vh;
+    flex-direction: column;
+    background-color: aliceblue;
+    margin: 5px;
+    border: 2px solid lightgray;
+    flex: 2;
+  }
+  
+  .top-wrapper{
+    display: flex;
+  }
 
+  .ul-tags{
+    font-size: 15px;
+  }
+
+  li{
+    text-decoration: none;
+  }
+.image-map{
+  left: 0;
+  top: 0;
+  max-width: 40vw;
+  max-height: 37vh;
+}
+.serial-number{
+  color: red;
+}
+
+.tags{
+  color: violet;
+}
+.tags-li{
+  color: yellowgreen;
+  list-style-type: none;
+}
+
+.location-address{
+  color: blue;
+}
+.btn_schedule {
+  background-color: rgb(211, 211, 211);
+  border-radius: 15px;
+  width: 100px;
+  height: 50px;
+}
+
+.btn_schedule :hover{
+  background-color: aqua;
+}
+
+.floor{
+  color: rgb(0, 0, 0);
+  font-weight: bold;
+
+}
+.trade-point{
+  color: tomato;
+}
 </style>
--- src/components/schedule-modal.vue
+++ src/components/schedule-modal.vue
@@ -14,15 +14,26 @@ defineEmits<{
 
 <template>
   <div class="schedule-modal">
-    <p>Working Time</p>
+    <p>Время работы</p>
     <ul>
-      <li :key="day" v-for="(value, day) in schedule">
+      <li class="li" :key="day" v-for="(value, day) in schedule">
         {{ day }}: {{ value }}
       </li>
     </ul>
 
-    <button @click="$emit('close')">close</button>
+    <button class="btn" @click="$emit('close')">Закрыть</button>
   </div>
 </template>
 
-<style></style>
+<style scoped>
+ .btn{
+  text-align: center;
+  width: 80px;
+  height: 30px;
+  border-radius: 8px;
+ }
+
+ .li{
+  list-style-type: none;
+ }
+</style>
